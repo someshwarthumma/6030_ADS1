6b7e7810fc7861520714c15e0eda7353a44359d1
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-09-27 19:06:14 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- Assignments/m4/4.1 Balanced Paranthesis/Solution.java
+++ Assignments/m4/4.1 Balanced Paranthesis/Solution.java
@@ -1,81 +1,81 @@
 import java.util.Scanner;
 import java.util.Arrays;
-class Stack{
+class Stack {
 	char[] data;
 	int size;
-	Stack(){
+	Stack() {
 		data = new char[20];
 		size = 0;
 	}
-	public void push(char s){
-		if(size == data.length){
+	public void push(char s) {
+		if (size == data.length) {
 			resize();
 		}
-		if(s == '('){
+		if (s == '(') {
 			data[size++] = ')';
-		} else if(s == '['){
+		} else if (s == '[') {
 			data[size++] = ']';
 		} else {
 			data[size++] = '}';
 		}
 	}
-	public char pop(){
-		if(size == 0){
+	public char pop() {
+		if (size == 0) {
 			return '0';
 		}
 		size--;
 		return data[size];
 	}
-	public boolean isEmpty(){
-		return size==0;
+	public boolean isEmpty() {
+		return size == 0;
 	}
-	private void resize(){
-		data = Arrays.copyOf(data , 2*data.length);
+	private void resize() {
+		data = Arrays.copyOf(data , 2 * data.length);
 	}
-	public void setSize(){
-		this.size = 0; 
+	public void setSize() {
+		this.size = 0;
 	}
 }
 
 
-public class Solution{
+public class Solution {
 	public static void main(String[] args) {
 		Stack stackObj = new Stack();
 		Scanner scan = new Scanner(System.in);
 		int n = Integer.parseInt(scan.nextLine());
-		for(int i=0;i<n;i++){
+		for (int i = 0; i < n; i++) {
 			boolean flag = true;
 			char[] s = scan.nextLine().toCharArray();
-			if(s.length %2 ==1){
+			if (s.length % 2 == 1) {
 				flag = false;
 				System.out.println("NO");
 				break;
 			}
-			for(char each: s){
-				if(s[s.length-1] == '[' || s[s.length-1] == '{' || s[s.length-1] == '(' ){
+			for (char each : s) {
+				if (s[s.length - 1] == '[' || s[s.length - 1] == '{' || s[s.length - 1] == '(' ) {
 					System.out.println("NO");
 					flag = false;
 					break;
 				}
-			if(each == '(' || each == '{' || each == '['){
-				stackObj.push(each);
-			}else{
-				if(each != stackObj.pop()){
-					stackObj.setSize();
-					System.out.println("NO");
-					flag = false;
-					break;
+				if (each == '(' || each == '{' || each == '[') {
+					stackObj.push(each);
+				} else {
+					if (each != stackObj.pop()) {
+						stackObj.setSize();
+						System.out.println("NO");
+						flag = false;
+						break;
+					}
 				}
-			}
-			
 
-		}
-		if(flag){
-				System.out.println("NO");				
+
 			}
-			
-		
-			
+			if (flag) {
+				System.out.println("YES");
+			}
+
+
+
 		}
 	}
 }
