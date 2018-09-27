e7a96ebec92cd5fde990d99318618d424e6e1cfa
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-09-27 17:36:06 +0530

    "check_style_score -> 3 of 4 passed. style score: 0/1 "

---
+++ Assignments/m4/4.1 Balanced Paranthesis/Solution.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+import java.util.Arrays;
+class Stack{
+	char[] data;
+	int size;
+	Stack(){
+		data = new char[20];
+		size = 0;
+	}
+	public void push(char s){
+		if(size == data.length){
+			resize();
+		}
+		if(s == '('){
+			data[size++] = ')';
+		} else if(s == '['){
+			data[size++] = ']';
+		} else {
+			data[size++] = '}';
+		}
+	}
+	public char pop(){
+		if(size == 0){
+			return '0';
+		}
+		size--;
+		return data[size];
+	}
+	public boolean isEmpty(){
+		return size==0;
+	}
+	private void resize(){
+		data = Arrays.copyOf(data , 2*data.length);
+	}
+	public void setSize(){
+		this.size = 0; 
+	}
+}
+
+
+public class Solution{
+	public static void main(String[] args) {
+		Stack stackObj = new Stack();
+		Scanner scan = new Scanner(System.in);
+		int n = Integer.parseInt(scan.nextLine());
+		for(int i=0;i<n;i++){
+			boolean flag = true;
+			char[] s = scan.nextLine().toCharArray();
+			for(char each: s){
+			if(each == '(' || each == '{' || each == '['){
+				stackObj.push(each);
+			}else{
+				if(each != stackObj.pop()){
+					stackObj.setSize();
+					System.out.println("NO");
+					flag = false;
+					break;
+				}
+			}
+			
+
+		}
+		if(flag){
+			System.out.println("YES");
+		}
+			
+		}
+	}
+}
