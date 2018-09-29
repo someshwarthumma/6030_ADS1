0e7fd9156ddb90593178d66cd6793ee4aa2870e0
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-09-29 18:18:01 +0530

    "check_style_score -> 4 of 5 passed. style score: 0/1 "

--- Assignments/m5/5.1 Deque/Solution.java
+++ Assignments/m5/5.1 Deque/Solution.java
@@ -1,16 +1,39 @@
 import java.util.Scanner;
+/**.
+ * solution class 
+ */
 public class Solution{
-	public static void main(String[] args) {
+	/**.
+	 * This is constructor.
+	 */
+	private Solution(){
+		//Solution constructor
+	}
+	/**.
+	 * Main method to handle the tescases
+	 *
+	 * @param      args  arguments
+	 */
+	public static void main(final String[] args) {
+		/**.
+		 * Creating the Deque class object for deque operations.
+		 */
 		Deque<Integer> d = new Deque<Integer>();
+		/**.
+		 * Scanner object to read the inputs.
+		 */
 		Scanner scan = new Scanner(System.in);
 		while(scan.hasNext()){
+			/**.
+			 * reading inputs into the string array tokens
+			 */
 			String[] tokens = scan.nextLine().split(" ");
 			switch(tokens[0]){
 				case "pushLeft":
 					d.pushLeft(Integer.parseInt(tokens[1]));
 					break;
 				case "pushRight":
-					d.pushRight(Integer.parseInt(tokens[1]));
+					d.pushRight(Integer.parseInt(tokens[1]));  
 					break;
 				case "size":
 					System.out.println(d.size());
@@ -24,6 +47,7 @@ public class Solution{
 				case "popRight":
 					d.popRight();
 					break;
+				default:
 			}
 		}
 	}
