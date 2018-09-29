586f0a6813a6cf3d8b5e9f81c4e59eaddadd7a67
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-09-29 18:42:06 +0530

    "check_style_score -> 4 of 5 passed. style score: 0/1 "

--- Assignments/m5/5.1 Deque/Solution.java
+++ Assignments/m5/5.1 Deque/Solution.java
@@ -1,30 +1,39 @@
 import java.util.Scanner;
-public class Solution{
-	public static void main(String[] args) {
-		Deque<Integer> d = new Deque<Integer>();
-		Scanner scan = new Scanner(System.in);
-		while(scan.hasNext()){
-			String[] tokens = scan.nextLine().split(" ");
-			switch(tokens[0]){
-				case "pushLeft":
-					d.pushLeft(Integer.parseInt(tokens[1]));
-					break;
-				case "pushRight":
-					d.pushRight(Integer.parseInt(tokens[1]));
-					break;
-				case "size":
-					System.out.println(d.size());
-					break;
-				case "isEmpty":
-					System.out.println(d.isEmpty());
-					break;
-				case "popLeft":
-					d.popLeft();
-					break;
-				case "popRight":
-					d.popRight();
-					break;
-			}
-		}
-	}
+/**.
+ * Solution class
+ */
+public class Solution {
+    /**.
+     * Main method to handle the test cases
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Deque<Integer> d = new Deque<Integer>();
+        Scanner scan = new Scanner(System.in);
+        while (scan.hasNext()) {
+            String[] tokens = scan.nextLine().split(" ");
+            switch (tokens[0]) {
+            case "pushLeft":
+                d.pushLeft(Integer.parseInt(tokens[1]));
+                break;
+            case "pushRight":
+                d.pushRight(Integer.parseInt(tokens[1]));
+                break;
+            case "size":
+                System.out.println(d.size());
+                break;
+            case "isEmpty":
+                System.out.println(d.isEmpty());
+                break;
+            case "popLeft":
+                d.popLeft();
+                break;
+            case "popRight":
+                d.popRight();
+                break;
+            default:
+            }
+        }
+    }
 }
