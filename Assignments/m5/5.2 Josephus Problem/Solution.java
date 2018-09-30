69825e444f7979db3c562b961933d30345e52335
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-09-30 11:50:30 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- Assignments/m5/5.2 Josephus Problem/Solution.java
+++ Assignments/m5/5.2 Josephus Problem/Solution.java
@@ -1,31 +1,55 @@
 import java.util.Scanner;
-public class Solution {
-	private Solution() {
-
-	}
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		Queue queueObj = new Queue();
-		int noOfInputs = Integer.parseInt(scan.nextLine());
-		for (int j = 0; j < noOfInputs; j++) {
-			String[] tokens = scan.nextLine().split(" ");
-			int number = Integer.parseInt(tokens[0]);
-			int position = Integer.parseInt(tokens[1]);
-			for (int i = 0; i < number; i++) {
-				queueObj.enQueue(i);
-			}
-			while (!queueObj.isEmpty() && number != 1) {
-				/*System.out.println("Size: "+queueObj.getSize());
-				System.out.println("while entered:");*/
-					for (int i = 1 ; i < position ; i++) {
-						queueObj.enQueue(queueObj.deQueue());
-					}
-					System.out.print(queueObj.deQueue()+" ");
-					number--;
-			}
-			System.out.print(queueObj.deQueue());
-			System.out.println();
-			//System.out.println("while completed");
-		}
-	}
+/**.
+ * Solution class
+ */
+public final class Solution {
+    /**.
+     * Sollution constructor
+     */
+    private Solution() {
+        //Solution constructor
+    }
+    /**.
+     * main method to handle the testcases
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        /**.
+         * scanner object to read the data
+         */
+        Scanner scan = new Scanner(System.in);
+        /**.
+         * Queue object to store the given data
+         */
+        Queue queueObj = new Queue();
+        /**.
+         * Variable to define the no of inputs
+         */
+        int noOfInputs = Integer.parseInt(scan.nextLine());
+        for (int index1 = 0; index1 < noOfInputs; index1++) {
+            String[] tokens = scan.nextLine().split(" ");
+            int number = Integer.parseInt(tokens[0]);
+            int position = Integer.parseInt(tokens[1]);
+            /**.
+             * adding given elements into the Queue.
+             */
+            for (int index = 0; index < number; index++) {
+                queueObj.enQueue(index);
+            }
+            /**.
+             * Checking of mth position person and poping the same
+             */
+            while (!queueObj.isEmpty() && number != 1) {
+                for (int i = 1 ; i < position ; i++) {
+                    queueObj.enQueue(queueObj.deQueue());
+                }
+                System.out.print(queueObj.deQueue() + " ");
+                number--;
+            }
+            System.out.print(queueObj.deQueue());
+            System.out.println();
+            //System.out.println("while completed");
+        }
+    }
 }
