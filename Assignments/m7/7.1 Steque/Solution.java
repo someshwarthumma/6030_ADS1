1258281f133e68b6293e0aa79758a23f6b8b4441
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-10-01 12:08:34 +0530

    "check_style_score -> 2 of 2 passed. style score: 0/1 "

--- Assignments/m7/7.1 Steque/Solution.java
+++ Assignments/m7/7.1 Steque/Solution.java
@@ -3,59 +3,59 @@ import java.util.Scanner;
  * Soluton class
  */
 public class Solution {
-	/**.
-	 * Solution constructor
-	 */
-	private Solution() {
-		//Constructor
-	}
-	/**.
-	 * Main method to handle the input testcases
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args) {
-		/**.
-		 * Scanner obj to read the inputs
-		 */
-		Scanner scan = new Scanner(System.in);
-		/**.
-		 * variable representing total no of inputs
-		 */
-		int noOfTests = Integer.parseInt(scan.nextLine());
-		/**.
-		 *eque object declaration as steque
-		 */
-		Steque steque = new Steque();
-		while (scan.hasNext()) {
-			/**.
-			 * reading input from console
-			 */
-			String[] input = scan.nextLine().split(" ");
-			/**.
-			 * resenting the steque object when input is not present
-			 */
-			if(input[0].length() ==0){
-				steque.reset();
-				System.out.println();
-			}
-			/**.
-			 * Switch case for different Operations
-			 */
-			switch (input[0]) {
-			case "push":
-				steque.push(Integer.parseInt(input[1]));
-				System.out.println(steque.printer());
-				break;
-			case "enqueue":
-				steque.enqueue(Integer.parseInt(input[1]));
-				System.out.println(steque.printer());
-				break;
-			case "pop":
-				steque.pop();
-				System.out.println(steque.printer());
-				break;
-			}
-		}
-	}
+    /**.
+     * Solution constructor
+     */
+    private Solution() {
+        //Constructor
+    }
+    /**.
+     * Main method to handle the input testcases
+     *
+     * @param      args  The arguments
+     */
+    public static void main(String[] args) {
+        /**.
+         * Scanner obj to read the inputs
+         */
+        Scanner scan = new Scanner(System.in);
+        /**.
+         * variable representing total no of inputs
+         */
+        int noOfTests = Integer.parseInt(scan.nextLine());
+        /**.
+         *eque object declaration as steque
+         */
+        Steque steque = new Steque();
+        while (scan.hasNext()) {
+            /**.
+             * reading input from console
+             */
+            String[] input = scan.nextLine().split(" ");
+            /**.
+             * resenting the steque object when input is not present
+             */
+            if (input[0].length() == 0) {
+                steque.reset();
+                System.out.println();
+            }
+            /**.
+             * Switch case for different Operations
+             */
+            switch (input[0]) {
+            case "push":
+                steque.push(Integer.parseInt(input[1]));
+                System.out.println(steque.printer());
+                break;
+            case "enqueue":
+                steque.enqueue(Integer.parseInt(input[1]));
+                System.out.println(steque.printer());
+                break;
+            case "pop":
+                steque.pop();
+                System.out.println(steque.printer());
+                break;
+            }
+        }
+    }
 }
