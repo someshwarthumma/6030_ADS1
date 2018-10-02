a88f6822f35a8c023f2ccdcd32d6ced2f6822482
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-10-02 21:42:17 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- Assignments/m8/8.2 Team Ranking With Selection/Solution.java
+++ Assignments/m8/8.2 Team Ranking With Selection/Solution.java
@@ -1,19 +1,29 @@
 import java.util.Scanner;
-class Solution{
-	private Solution(){
-		//This is Constructor
-	}
-
-	public static void main(String[] args) {
-		Leaderboard leaderboardObj = new Leaderboard();
-		Scanner scan = new Scanner(System.in);
-		while(scan.hasNext()){ 
-			String[] tokens = scan.nextLine().split(",");
-			leaderboardObj.add(new Teams(tokens[0], Integer.
-				parseInt(tokens[1]), Integer.
-				parseInt(tokens[2]), Integer.
-				parseInt(tokens[3])));
-		}
-		leaderboardObj.print();
-	}
+/**.
+ * Solution class
+ */
+class Solution {
+    /**.
+     * private constructor.
+     */
+    private Solution() {
+        //This is Constructor
+    }
+    /**.
+     * Main method to handle the input testacases
+     *
+     * @param      args  The arguments
+     */
+    public static void main(String[] args) {
+        Leaderboard leaderboardObj = new Leaderboard();
+        Scanner scan = new Scanner(System.in);
+        while (scan.hasNext()) {
+            String[] tokens = scan.nextLine().split(",");
+            leaderboardObj.add(new Teams(tokens[0], Integer.
+                parseInt(tokens[1]), Integer.
+                parseInt(tokens[2]), Integer.
+                parseInt(tokens[3])));
+        }
+        leaderboardObj.print();
+    }
 }
