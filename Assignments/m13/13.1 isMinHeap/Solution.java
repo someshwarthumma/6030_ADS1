a1d8f4528b8b13267eeae2c6b0ad1b96a788a19d
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-10-08 17:57:25 +0530

    "ads1_m13_a1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/m13/13.1 isMinHeap/Solution.java
+++ Assignments/m13/13.1 isMinHeap/Solution.java
@@ -1,81 +1,120 @@
 import java.util.Scanner;
-import java.util.Arrays;
 import java.lang.Comparable;
-class Solution{
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		String dataType = scan.nextLine();
-		int noOfTestCases = Integer.parseInt(scan.nextLine());
-		Check checkObj = new Check();
-		switch(dataType){
-			case "String":
-				while(scan.hasNext()){
-					String[] array = scan.nextLine().split(",");
-					String[] strArr = new String[array.length+1];
-					//System.out.println(Arrays.toString(stringArray));
-					int j =0;
-					for(int i=1;i<=array.length;i++){
-						strArr[i] = array[j];
-						j++;
-					}
-					//System.out.println(Arrays.toString(strArr));
-					System.out.println(checkObj.check(strArr));
-				}
-				break;
-			case "Integer":
-				while(scan.hasNext()){
-					String[] array = scan.nextLine().split(",");
-					Integer[] intArr = new Integer[array.length+1];
-					int j =0;
-					for(int i=1;i<=array.length;i++){
-						intArr[i] = Integer.parseInt(array[j]);
-						j++;
-					}
-					System.out.println(checkObj.check(intArr));
-				}
-				break;
-			case "Double":
-				while(scan.hasNext()){
-					String[] array = scan.nextLine().split(",");
-					Double[] doubleArr = new Double[array.length+1];
-					int j =0;
-					for(int i=1;i<=array.length;i++){
-						doubleArr[i] = Double.parseDouble(array[j]);
-						j++;
-					}
-					System.out.println(checkObj.check(doubleArr));
-				}
-				break;
-			case "Float":
-				for(int k =0;k<noOfTestCases;k++){
-					String array1 = scan.nextLine();
-					if(array1.length()==0){
-						System.out.println("false");
-						break;
-					}
-					String[] array = array1.split(",");
-					Float[] floatArr = new Float[array.length+1];
-					int j =0;
-					for(int i=1;i<=array.length;i++){
-						floatArr[i] = Float.parseFloat(array[j]);
-						j++;
-					}
-					System.out.println(checkObj.check(floatArr));
-				}
-				break;
-		}
-	}
+/**.
+ * Solution class
+ */
+class Solution {
+    /**.
+     * This is private constructor
+     */
+    private Solution(){
+        //constructor
+    }
+    /**.
+     * main method to handle input testcases
+     *
+     * @param      args  The arguments
+     */
+    public static void main(String[] args) {
+        /**.
+         * Scanner object reading
+         */
+        Scanner scan = new Scanner(System.in);
+        String dataType = scan.nextLine();
+        int noOfTestCases = Integer.parseInt(scan.nextLine());
+        MinPQ checkObj = new MinPQ();
+        /**
+         * input reading depending on the type of input
+         * complexity: O(N)
+         * string format is converted to respective dataType while reading
+         */
+        switch (dataType) {
+        case "String":
+            while (scan.hasNext()) {
+                String[] array = scan.nextLine().split(",");
+                String[] strArr = new String[array.length + 1];
+                int j = 0;
+                for (int i = 1; i <= array.length; i++) {
+                    strArr[i] = array[j];
+                    j++;
+                }
+                System.out.println(checkObj.check(strArr));
+            }
+            break;
+        case "Integer":
+            while (scan.hasNext()) {
+                String[] array = scan.nextLine().split(",");
+                Integer[] intArr = new Integer[array.length + 1];
+                int j = 0;
+                for (int i = 1; i <= array.length; i++) {
+                    intArr[i] = Integer.parseInt(array[j]);
+                    j++;
+                }
+                System.out.println(checkObj.check(intArr));
+            }
+            break;
+        case "Double":
+            while (scan.hasNext()) {
+                String[] array = scan.nextLine().split(",");
+                Double[] doubleArr = new Double[array.length + 1];
+                int j = 0;
+                for (int i = 1; i <= array.length; i++) {
+                    doubleArr[i] = Double.parseDouble(array[j]);
+                    j++;
+                }
+                System.out.println(checkObj.check(doubleArr));
+            }
+            break;
+        case "Float":
+            for (int k = 0; k < noOfTestCases; k++) {
+                String array1 = scan.nextLine();
+                if (array1.length() == 0) {
+                    System.out.println("false");
+                    break;
+                }
+                String[] array = array1.split(",");
+                Float[] floatArr = new Float[array.length + 1];
+                int j = 0;
+                for (int i = 1; i <= array.length; i++) {
+                    floatArr[i] = Float.parseFloat(array[j]);
+                    j++;
+                }
+                System.out.println(checkObj.check(floatArr));
+            }
+            break;
+        default:
+        }
+    }
 }
 
-class Check<E extends Comparable<E>>{ 
-
-	public boolean check(E[] arr){
-		int n=arr.length;
-		for(int i=n-1;i>1;i--){
-			if(arr[i].compareTo(arr[i/2]) < 0){
-				return false;
-			}
-		}
-		return true;
-	}
+/**.
+ * MinPQ class.
+ *
+ * @param      <E>   { Type generic argument }
+ */
+class MinPQ<E extends Comparable<E>> {
+    /**
+     * MinPQ constructor
+     */
+    //private MinPQ(){
+        //constructor
+    //}
+    /**.
+     * Check method to check weather it is minPQ or not
+     * Complexity is O(N)
+     * since we use For loop to check 
+     *
+     * @param      arr  array of generic type
+     *
+     * @return     { Boolean flag }
+     */
+    public boolean check(E[] arr) {
+        int n = arr.length;
+        for (int i = n - 1; i > 1; i--) {
+            if (arr[i].compareTo(arr[i / 2]) < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
