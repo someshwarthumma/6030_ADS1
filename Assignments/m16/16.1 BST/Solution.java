16a7954070e1185f32e216a8fb7762b9f9be447a
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-10-10 22:39:52 +0530

    "ads1_m16_a1: 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/m16/16.1 BST/Solution.java
+++ Assignments/m16/16.1 BST/Solution.java
@@ -3,95 +3,100 @@ import java.util.Scanner;
  * Solution class
  */
 class Solution {
-	/**.
-	 * { main method to handle the input testcases }
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		BinarySearchTree bst = new BinarySearchTree();
-		Scanner scan = new Scanner(System.in);
-		while (scan.hasNext()) {
-			String[] tokens = scan.nextLine().split(",");
-			switch (tokens[0]) {
-			case "put":
-				bst.put(new Book(tokens[1], tokens[2], tokens[3]), Integer.parseInt(tokens[4]));
-				break;
-			case "get":
-				int temp = bst.get(new Book(tokens[1], tokens[2], tokens[3]));
-				if(temp == 10101){
-					System.out.println("null");
-					break;
-				}
-				System.out.println(temp);
-				break;
-			default:
-			}
-		}
-	}
+    /**.
+     * { main method to handle the input testcases }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        BinarySearchTree bst = new BinarySearchTree();
+        Scanner scan = new Scanner(System.in);
+        final int one = 1;
+        final int two = 2;
+        final int three = 3;
+        final int four = 4;
+        final int nul = 10101;
+        while (scan.hasNext()) {
+            String[] tokens = scan.nextLine().split(",");
+            switch (tokens[0]) {
+            case "put":
+                bst.put(new Book(tokens[one], tokens[two], tokens[three]), Integer.parseInt(tokens[four]));
+                break;
+            case "get":
+                int temp = bst.get(new Book(tokens[one], tokens[two], tokens[three]));
+                if (temp == nul) {
+                    System.out.println("null");
+                    break;
+                }
+                System.out.println(temp);
+                break;
+            default:
+            }
+        }
+    }
 }
 /**.
  * Book class to store the propersties of a book
  */
 class Book implements Comparable<Book> {
-	/**.
-	 * variable for Book name
-	 */
-	private String name;
-	/**.
-	 * variable for name of the author
-	 */
-	private String author;
-	/**.
-	 * variable for storing the price of the book
-	 */
-	private double price;
-	/**.
-	 * Book class constructor
-	 *
-	 * @param      n     { name of type String }
-	 * @param      a     { Author of type  String }
-	 * @param      p     { Price of integer type }
-	 */
-	Book(final String n, final String a, final String p) {
-		this.name = n;
-		this.author = a;
-		this.price = Double.parseDouble(p);
-	}
-	/**.
-	 * method to get the name of the book
-	 *
-	 * @return     The name.
-	 */
-	public String getName() {
-		return name;
-	}
-	/*public String getAuthor() {
-		return author;
-	}*/
-	/*public double getPrice() {
-		return price;
-	}*/
-	/*public void setName(String n) {
-		this.name = n;
-	}
-	public void setAuthor(String a) {
-		this.author = a;
-	}
-	public void setPrice(String p) {
-		this.price = Double.parseDouble(p);
-	}*/
+    /**.
+     * variable for Book name
+     */
+    private String name;
+    /**.
+     * variable for name of the author
+     */
+    private String author;
+    /**.
+     * variable for storing the price of the book
+     */
+    private double price;
+    /**.
+     * Book class constructor
+     *
+     * @param      n     { name of type String }
+     * @param      a     { Author of type  String }
+     * @param      p     { Price of integer type }
+     */
+    Book(final String n, final String a, final String p) {
+        this.name = n;
+        this.author = a;
+        this.price = Double.parseDouble(p);
+    }
+    /**.
+     * method to get the name of the book
+     *
+     * @return     The name.
+     */
+    public String getName() {
+        return name;
+    }
+    /*public String getAuthor() {
+        return author;
+    }*/
+    /*public double getPrice() {
+        return price;
+    }*/
+    /*public void setName(String n) {
+        this.name = n;
+    }
+    public void setAuthor(String a) {
+        this.author = a;
+    }
+    public void setPrice(String p) {
+        this.price = Double.parseDouble(p);
+    }*/
 
-	/**.
-	 * method to compare the given two books
-	 *
-	 * @param      that  book as Type Book
-	 *
-	 * @return     { Integer value for comparision }
-	 */
-	public int compareTo(final Book that) {
-		return this.getName().compareTo(that.getName());
-	}
+    /**.
+     * method to compare the given two books
+     *
+     * @param      that  book as Type Book
+     *
+     * @return     { Integer value for comparision }
+     */
+    public int compareTo(final Book that) {
+        return this.getName().compareTo(that.getName());
+    }
 }
 /**.
  * BinarySearchTree class
@@ -100,122 +105,123 @@ class Book implements Comparable<Book> {
  * @param      <Integer>  value of type Integer
  */
 class BinarySearchTree<Book extends Comparable<Book>, Integer> {
-	/**.
-	 * variable to deneote the root of the bst
-	 */
-	private Node root;
-	/**.
-	 * Node class to denote the the BST structure which contains key, value
-	 */
-	class Node {
-		/**.
-		 * variable for key of type book
-		 */
-		public Book key;
-		/**.
-		 * variable for Value of type int 
-		 */
-		public int value;
-		/**.
-		 * variable left leaf as Left of type Node
-		 */
-		public Node left;
-		/**.
-		 * Varaible for right leaf as Right of type Node
-		 */
-		public Node right;
-		/**.
-		 * Node constructor
-		 *
-		 * @param      k     { of tyoe Book }
-		 * @param      v     { of type integer }
-		 */
-		Node(Book k, int v) {
-			this.key = k;
-			this.value = v;
-			this.right = null;
-			this.left = null;
-		}
-		/**.
-		 * Method to return the key
-		 *
-		 * @return     The key.
-		 */
-		public Book getKey() {
-			return this.key;
-		}
-		/**.
-		 * method to return value
-		 *
-		 * @return     The value.
-		 */
-		public int getValue() {
-			return this.value;
-		}
-	}
-	/**.
-	 * constructor for BinarySearchTree
-	 */
-	BinarySearchTree() {
-		this.root = null;
-	}
-	/**.
-	 * method for putting a key into the bst
-	 *
-	 * @param      key    of type book
-	 * @param      value  of type Integer
-	 */
-	public void put(final Book key, final int value) {
-		root = put(root, key, value);
-	}
-	/**.
-	 * method to put the given key into the BST recursively.
-	 * 
-	 *
-	 * @param      curr   of type Node
-	 * @param      key    of type Book
-	 * @param      value  of type integer
-	 *
-	 * @return     { Current node of type Node }
-	 */
-	public Node put(final Node curr, final Book key, final int value) {
-		Node current = curr;
+    /**.
+     * variable to deneote the root of the bst
+     */
+    private Node root;
+    /**.
+     * Node class to denote the the BST structure which contains key, value
+     */
+    class Node {
+        /**.
+         * variable for key of type book
+         */
+        public Book key;
+        /**.
+         * variable for Value of type int
+         */
+        public int value;
+        /**.
+         * variable left leaf as Left of type Node
+         */
+        public Node left;
+        /**.
+         * Varaible for right leaf as Right of type Node
+         */
+        public Node right;
+        /**.
+         * Node constructor
+         *
+         * @param      k     { of tyoe Book }
+         * @param      v     { of type integer }
+         */
+        Node(Book k, int v) {
+            this.key = k;
+            this.value = v;
+            this.right = null;
+            this.left = null;
+        }
+        /**.
+         * Method to return the key
+         *
+         * @return     The key.
+         */
+        public Book getKey() {
+            return this.key;
+        }
+        /**.
+         * method to return value
+         *
+         * @return     The value.
+         */
+        public int getValue() {
+            return this.value;
+        }
+    }
+    /**.
+     * constructor for BinarySearchTree
+     */
+    BinarySearchTree() {
+        this.root = null;
+    }
+    /**.
+     * method for putting a key into the bst
+     *
+     * @param      key    of type book
+     * @param      value  of type Integer
+     */
+    public void put(final Book key, final int value) {
+        root = put(root, key, value);
+    }
+    /**.
+     * method to put the given key into the BST recursively.
+     *
+     *
+     * @param      curr   of type Node
+     * @param      key    of type Book
+     * @param      value  of type integer
+     *
+     * @return     { Current node of type Node }
+     */
+    public Node put(final Node curr, final Book key, final int value) {
+        Node current = curr;
 
-		if(current == null){
-			return new Node(key, value);
-		}
-		int c = key.compareTo(current.key);
-		if (c < 0) {
-			current.left = put(current.left, key, value);
-		} else if (c > 0) {
-			current.right = put(current.right, key, value);
-		} else if ( c == 0) {
-			current.value = value;
-		}
-		return current;
+        if (current == null) {
+            return new Node(key, value);
+        }
+        int c = key.compareTo(current.key);
+        if (c < 0) {
+            current.left = put(current.left, key, value);
+        } else if (c > 0) {
+            current.right = put(current.right, key, value);
+        } else if ( c == 0) {
+            current.value = value;
+        }
+        return current;
 
-	}
-	/**.
-	 * { method to get the value for the searched element }
-	 *
-	 * @param      key   of type key
-	 *
-	 * @return     { the value of type integer }
-	 */
-	public int get(final Book key){
-		Node current = root;
-		while(current != null){
-			int c = key.compareTo(current.key);
-			if(c>0){
-				current = current.right;
-			} else if(c <0){
-				current = current.left;
-			} else if ( c == 0){
-				return current.value;
-			}
-		}
-		return 10101;
-	}
+    }
+    /**.
+     * { method to get the value for the searched element }
+     *
+     * @param      key   of type key
+     *
+     * @return     { the value of type integer }
+     */
+    public int get(final Book key) {
+        Node current = root;
+        while (current != null) {
+            int c = key.compareTo(current.key);
+            if (c > 0) {
+                current = current.right;
+            } else if (c < 0) {
+                current = current.left;
+            } else if ( c == 0) {
+                return current.value;
+            }
+        }
+        final int nul = 10101;
+        return nul;
+    }
 
 
 }
