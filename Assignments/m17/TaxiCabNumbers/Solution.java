372e97c768724ba7d0535545dc680dbf90810dd2
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-10-12 16:08:34 +0530

    "taxi_cab_numbers: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/m17/TaxiCabNumbers/Solution.java
+++ Assignments/m17/TaxiCabNumbers/Solution.java
@@ -22,3 +22,55 @@ class Solution {
 		System.out.println(result);
 	}
 }
+
+/**.
+ * TaxiCabNumbers class.
+ */
+class TaxiCabNumbers {
+    /**.
+     * method to find the taxiNumber.
+     *
+     * @param      number        The number of type integer
+     * @param      possibleWays  of type integer
+     *
+     * @return     { Integer type which is a taxiCabNumber }
+     */
+    public int findTaxiCabNumber(final int number, final int possibleWays) {
+        int num = number;
+        int ways = possibleWays;
+        final int n = 600;
+
+        // initialize priority queue
+        MinPQ<CubeSum> pq = new MinPQ<CubeSum>();
+        for (int i = 0; i <= n; i++) {
+            pq.insert(new CubeSum(i, i));
+        }
+        int previous = -1;
+        int numCounter = 0;
+        int waysCounter = 1;
+        // find smallest sum, print it out, and update
+
+        while (!pq.isEmpty()) {
+            CubeSum s = pq.delMin();
+            //System.out.println("This is while: ");
+            if (previous == s.sum) {
+                waysCounter++;
+            } else {
+                waysCounter = 0;
+            }
+
+            if (waysCounter == ways - 1) {
+                numCounter++;
+                if (numCounter == num) {
+                    return s.sum;
+                }
+            }
+            previous = s.sum;
+            //StdOut.println(s);
+            if (s.j < n) {
+                pq.insert(new CubeSum(s.i, s.j + 1));
+            }
+        }
+        return -1;
+    }
+}
