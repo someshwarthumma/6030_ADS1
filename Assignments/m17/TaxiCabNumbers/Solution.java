1a25cf96643266f78c742c87cd624543dc5ac624
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-10-12 16:10:37 +0530

    "taxi_cab_numbers: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/m17/TaxiCabNumbers/Solution.java
+++ Assignments/m17/TaxiCabNumbers/Solution.java
@@ -1,26 +1,28 @@
 import java.util.Scanner;
-
+/**
+ * solution class
+ */
 class Solution {
-	/**
-	 * solution constructor.
-	 */
-	private Solution(){
-	//Solution constructor
-	}
-	/**
-	 * method to handle the input testcases
-	 *
-	 * @param      args  argument
-	 */
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		String[] tokens = scan.nextLine().split(" ");
-		TaxiCabNumbers tcn = new TaxiCabNumbers();
-		int result = tcn.findTaxiCabNumber(Integer.
-			parseInt(tokens[0]), Integer.
-			parseInt(tokens[1]));
-		System.out.println(result);
-	}
+    /**.
+     * solution constructor.
+     */
+    private Solution() {
+        //Solution constructor
+    }
+    /**.
+     * method to handle the input testcases
+     *
+     * @param      args  argument
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String[] tokens = scan.nextLine().split(" ");
+        TaxiCabNumbers tcn = new TaxiCabNumbers();
+        int result = tcn.findTaxiCabNumber(Integer.
+                                           parseInt(tokens[0]), Integer.
+                                           parseInt(tokens[1]));
+        System.out.println(result);
+    }
 }
 
 /**.
