648da85c4df5921e51090b9ce8db6cc1692ea256
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-10-13 13:03:20 +0530

    "ads1_week3_exam: 1/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/m18/Week-3_Assignment-1/Solution.java
+++ Assignments/m18/Week-3_Assignment-1/Solution.java
@@ -1,74 +1,86 @@
 import java.util.Scanner;
+/**.
+ * solution class
+ */
 class Solution {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int noOfStocks = Integer.parseInt(scan.nextLine());
-		
-		for (int j = 0; j < 6; j++) {
-			//for (int j = 0; j < noOfStocks; j++) {
+    /**.
+     * a
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int noOfStocks = Integer.parseInt(scan.nextLine());
 
-			MinPQ<Stock> maxST = new MinPQ<Stock>();
-			MaxPQ<Stock> minST = new MaxPQ<Stock>();
-			float median = 0.0f;
+        for (int j = 0; j < 6; j++) {
+            //for (int j = 0; j < noOfStocks; j++) {
 
-			for (int i = 0; i < noOfStocks; i++) {
+            MinPQ<Stock> maxST = new MinPQ<Stock>();
+            MaxPQ<Stock> minST = new MaxPQ<Stock>();
+            float median = 0.0f;
 
-				String[] stockData = scan.nextLine().split(",");
-				Stock stockObj = new Stock(stockData[0], Float.parseFloat(stockData[1]));
-				float k = stockObj.getChange();
-				if (k > median) {
-					maxST.insert(stockObj);
-				} else {
-					minST.insert(stockObj);
-				}
-				if (minST.size() - maxST.size() > 1) {
-					Stock x = minST.delMax();
-					maxST.insert(x);
-				}
-				if (maxST.size() - minST.size() > 1) {
-					Stock y = maxST.delMin();
-					minST.insert(y);
-				}
-				if (maxST.size() == minST.size()) {
-					median = (maxST.min().getChange() + minST.max().getChange()) / 2;
-					//System.out.println(median);
-				}
-				if (minST.size() > maxST.size()) {
-					median = minST.max().getChange();
-					//System.out.println(median);
-				}
-				if (maxST.size() > minST.size()) {
-					median = maxST.min().getChange();
-					//System.out.println(median);
-				}
-			}
+            for (int i = 0; i < noOfStocks; i++) {
 
-			Stock[] maxStocks = new Stock[maxST.size()];
-			Stock[] minStocks = new Stock[minST.size()];
-			for(int i= 0; i< maxStocks.length;i++){
-				maxStocks[i] = maxST.delMin();
-			}
-			for(int i= 0; i< minStocks.length;i++){
-				minStocks[i] = minST.delMax();
-			}
-			printer(maxStocks);
-			printer(minStocks);
-		}
-	}
+                String[] stockData = scan.nextLine().split(",");
+                Stock stockObj = new Stock(stockData[0], Float.parseFloat(stockData[1]));
+                float k = stockObj.getChange();
+                if (k > median) {
+                    maxST.insert(stockObj);
+                } else {
+                    minST.insert(stockObj);
+                }
+                if (minST.size() - maxST.size() > 1) {
+                    Stock x = minST.delMax();
+                    maxST.insert(x);
+                }
+                if (maxST.size() - minST.size() > 1) {
+                    Stock y = maxST.delMin();
+                    minST.insert(y);
+                }
+                if (maxST.size() == minST.size()) {
+                    median = (maxST.min().getChange() + minST.max().getChange()) / 2;
+                    //System.out.println(median);
+                }
+                if (minST.size() > maxST.size()) {
+                    median = minST.max().getChange();
+                    //System.out.println(median);
+                }
+                if (maxST.size() > minST.size()) {
+                    median = maxST.min().getChange();
+                    //System.out.println(median);
+                }
+            }
 
-	public static void printer(Stock[] s){
-		int counter = 0;
-		for(int i = s.length-1;i>=0;i--){
-			System.out.println(s[i].getName()+" "+s[i].getChange());
-			Stock st = s[i];
-			
-			counter++;
-			if(counter == 5){
-				break;
-			}
-		}
-		System.out.println();
-	}
+            Stock[] maxStocks = new Stock[maxST.size()];
+            Stock[] minStocks = new Stock[minST.size()];
+            for (int i = 0; i < maxStocks.length; i++) {
+                maxStocks[i] = maxST.delMin();
+            }
+            for (int i = 0; i < minStocks.length; i++) {
+                minStocks[i] = minST.delMax();
+            }
+            printer(maxStocks);
+            printer(minStocks);
+        }
+    }
+    /**.
+     * method to print the output.
+     *
+     * @param      s     { parameter_description }
+     */
+    public static void printer(final Stock[] s) {
+        int counter = 0;
+        for (int i = s.length - 1; i >= 0; i--) {
+            System.out.println(s[i].getName() + " " + s[i].getChange());
+            Stock st = s[i];
+
+            counter++;
+            if (counter == 5) {
+                break;
+            }
+        }
+        System.out.println();
+    }
 }
 
 
--- Assignments/m18/Week-3_Assignment-1/Stock.java
+++ Assignments/m18/Week-3_Assignment-1/Stock.java
@@ -1,27 +1,57 @@
-class Stock implements Comparable<Stock>{
-	String name;
-	Float change;
-	Stock(String s, Float c){
-		this.name = s;
-		this.change = c;
-	}
-
-	public String getName(){
-		return this.name;
-	}
-	public Float getChange(){
-		return this.change;
-	}
-
-	public int compareTo(Stock that){
-		if(this.getChange() != that.getChange()){
-			return this.getChange().compareTo(that.getChange());
-		}
-		Integer s1 = this.getName().length();
-		Integer s2 = that.getName().length();
-		if(s1 != s2){
-			return s1.compareTo(s2);
-		}
-		return that.getName().compareTo(this.getName());
-	}
+/**
+ * Stock class
+ */
+class Stock implements Comparable<Stock> {
+    /**
+     * variable for name
+     */
+    private String name;
+    /**
+     * variable for change in stock
+     */
+    private Float change;
+    /**.
+     * constructor of Stock class
+     *
+     * @param      s     { parameter_description }
+     * @param      c     { parameter_description }
+     */
+    Stock(String s, Float c) {
+        this.name = s;
+        this.change = c;
+    }
+    /**
+     * getter for name
+     *
+     * @return     The name.
+     */
+    public String getName() {
+        return this.name;
+    }
+    /**
+     * getter for change
+     *
+     * @return     The change.
+     */
+    public Float getChange() {
+        return this.change;
+    }
+    /**
+     * compareTo method for comparision of objects.
+     *
+     * @param      that  The that
+     *
+     * @return     { Integer }
+     */
+    public int compareTo(final Stock that) {
+        if (this.getChange() != that.getChange()) {
+            return this.getChange().compareTo(that.getChange());
+        }
+        Integer s1 = this.getName().length();
+        Integer s2 = that.getName().length();
+        if (s1 != s2) {
+            return s1.compareTo(s2);
+        }
+        return that.getName().compareTo(this.getName());
+    }
 }
