2f312193cf5dd6cb5648a261e09fe52dd3a06aba
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-10-23 16:34:45 +0530

    "ads1_m5_a1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- Assignments/m5/5.1 Deque/Deque.java
+++ Assignments/m5/5.1 Deque/Deque.java
@@ -1,89 +1,89 @@
-/**
+/**.
  * Dequeue class for the implimentaion of dequeue
  *
  * @param      <E>   { Generic }
  */
-public class Deque<E>{
-	/**.
-	 * Size variable
-	 */
-	int size;
-	/**.
-	 * Object for linked list 
-	 */
-	LinkedList<E> linkedQueue;
-	Deque(){
-		linkedQueue = new LinkedList<E>();
-		size =0;
-	}
-	/**.
-	 * is empty method to check weather dequeqe is empty or not
-	 *
-	 * @return     True if empty, False otherwise.
-	 */
-	public boolean isEmpty(){
-		return size == 0;
-	}
-	/**.
-	 * Size method to know the size
-	 *
-	 * @return     { int }
-	 */
-	public int size(){
-		return size;
-	}
-	/**.
-	 * Push left method to put the element at the left end
-	 *
-	 * @param      item  The item
-	 */
-	public void pushLeft(final E item){
-		linkedQueue.addAtStart(item);
-		size++;
-		linkedQueue.print();
-	}
-	/**.
-	 * push right method to push the element in right end
-	 *
-	 * @param      item  The item
-	 */
-	public void pushRight(final E item){
-		linkedQueue.addAtEnd(item);
-		size++;
-		linkedQueue.print();
-	}
-	/**.
-	 * remove the elemenet from left end
-	 */
-	public void popLeft(){
-		if(size==0){
-			System.out.println("Deck is empty");
-			return;
-		}
-		size--;
-		linkedQueue.removeAtStart();
-		if(linkedQueue.getSize()==0) {
-			System.out.println("[]");
-			return;
-		}
-		linkedQueue.print();
-	}
-	/**.
-	 * removing from right end;
-	 */
-	public void popRight(){
-		if(size==0){
-			System.out.println("Deck is empty");
-			return;
-		}
-		size--;
-		linkedQueue.removeAtEnd();
-		if(linkedQueue.getSize()==0) {
-			System.out.println("[]");
-			return;
-		}
-		linkedQueue.print();
-	}
+public class Deque<E> {
+    /**.
+     * Size variable
+     */
+    int size;
+    /**.
+     * Object for linked list
+     */
+    LinkedList<E> linkedQueue;
+    Deque() {
+        linkedQueue = new LinkedList<E>();
+        size = 0;
+    }
+    /**.
+     * is empty method to check weather dequeqe is empty or not
+     *
+     * @return     True if empty, False otherwise.
+     */
+    public boolean isEmpty() {
+        return size == 0;
+    }
+    /**.
+     * Size method to know the size
+     *
+     * @return     { int }
+     */
+    public int size() {
+        return size;
+    }
+    /**.
+     * Push left method to put the element at the left end
+     *
+     * @param      item  The item
+     */
+    public void pushLeft(final E item) {
+        linkedQueue.addAtStart(item);
+        size++;
+        linkedQueue.print();
+    }
+    /**.
+     * push right method to push the element in right end
+     *
+     * @param      item  The item
+     */
+    public void pushRight(final E item) {
+        linkedQueue.addAtEnd(item);
+        size++;
+        linkedQueue.print();
+    }
+    /**.
+     * remove the elemenet from left end
+     */
+    public void popLeft() {
+        if (size == 0) {
+            System.out.println("Deck is empty");
+            return;
+        }
+        size--;
+        linkedQueue.removeAtStart();
+        if (linkedQueue.getSize() == 0) {
+            System.out.println("[]");
+            return;
+        }
+        linkedQueue.print();
+    }
+    /**.
+     * removing from right end;
+     */
+    public void popRight() {
+        if (size == 0) {
+            System.out.println("Deck is empty");
+            return;
+        }
+        size--;
+        linkedQueue.removeAtEnd();
+        if (linkedQueue.getSize() == 0) {
+            System.out.println("[]");
+            return;
+        }
+        linkedQueue.print();
+    }
 
 
 
