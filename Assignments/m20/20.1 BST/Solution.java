937188971a9fdb2e84749be70ce8b2f0b2aeb017
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-10-24 13:55:46 +0530

    ": 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/m20/20.1 BST/Solution.java
+++ Assignments/m20/20.1 BST/Solution.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 /**.
  * This solution class for input testcases
  */
-class Solution{
-	/**.
-	 * Main method
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
+class Solution {
+    /**.
+     * Main method
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
         Scanner scan = new Scanner(System.in);
         BST bst = new BST();
         while (scan.hasNext()) {
@@ -16,12 +16,12 @@ class Solution{
             switch (tokens[0]) {
             case "put":
                 bst.put(new BookInfo(tokens[1], tokens[2],
-                tokens[2 + 1]), Integer.parseInt(tokens[2 + 2]));
+                    tokens[2 + 1]), Integer.parseInt(tokens[2 + 2]));
                 break;
             case "get":
                 int output = bst.get(new BookInfo(tokens[1],
-                    tokens[2],
-                                   tokens[2 + 1]));
+                                                  tokens[2],
+                                                  tokens[2 + 1]));
                 if (output == 0) {
                     System.out.println("null");
                 } else {
@@ -38,12 +38,12 @@ class Solution{
                 break;
             case "select":
                 Comparable select = bst.select(Integer.
-                        parseInt(tokens[1]));
+                    parseInt(tokens[1]));
                 System.out.println(select.toString());
                 break;
             case "floor":
                 Comparable floor = bst.floor(new BookInfo(
-                        tokens[1], tokens[2], tokens[2 + 1]));
+                    tokens[1], tokens[2], tokens[2 + 1]));
                 if (floor == null) {
                     System.out.println("null");
                 } else {
@@ -52,7 +52,7 @@ class Solution{
                 break;
             case "ceiling":
                 Comparable ceiling = bst.ceiling(new BookInfo(
-                            tokens[1], tokens[2], tokens[2 + 1]));
+                    tokens[1], tokens[2], tokens[2 + 1]));
                 if (ceiling == null) {
                     System.out.println("null");
                 } else {
@@ -75,69 +75,69 @@ class Solution{
         }
     }
 }
-/**
+/**.
  * Class to represent the properties of a book
  */
 class BookInfo implements Comparable<BookInfo> {
-    /**
+    /**.
      * variable for bookName
      */
-	String bookName;
-    /**
+    String bookName;
+    /**.
      * variable for author name
      */
-	String bookAuthor;
-    /**
+    String bookAuthor;
+    /**.
      * variable for price
      */
-	String bookPrice;
-    /**
+    String bookPrice;
+    /**.
      * BookInfo constructor
      *
      * @param      n     { as name }
      * @param      a     { as Author }
      * @param      p     { as price }
      */
-	BookInfo(final String n, final String a, final String p){
-		this.bookName = n;
-		this.bookAuthor = a;
-		this.bookPrice = p;
-	}
-    /**
+    BookInfo(final String n, final String a, final String p) {
+        this.bookName = n;
+        this.bookAuthor = a;
+        this.bookPrice = p;
+    }
+    /**.
      * method to get the name of the book
      *
      * @return     The name of type string
      */
-	public String getName(){
-		return this.bookName;
-	}
-    /**
+    public String getName() {
+        return this.bookName;
+    }
+    /**.
      * method to get Author  of the book
      *
      * @return     The author of type string
      */
-	public String getAuthor(){
-		return this.bookAuthor;
-	}
-    /**
+    public String getAuthor() {
+        return this.bookAuthor;
+    }
+    /**.
      * method to get the price of the book
      *
      * @return     The price of type string
      */
-	public String getPrice(){
-		return this.bookPrice;
-	}
-    /**
+    public String getPrice() {
+        return this.bookPrice;
+    }
+    /**.
      * Returns a string representation of the object.
      *
      * @return     String representation of the object.
      */
-	public String toString() {
+    public String toString() {
         return this.getName() + ", "
                + this.getAuthor() + ", "
                + Float.parseFloat(this.getPrice());
     }
-    /**
+    /**.
      * method to compare the given two books
      *
      * @param      that  object
