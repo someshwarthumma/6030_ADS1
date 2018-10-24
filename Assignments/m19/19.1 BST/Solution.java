eb9a28b81c571f2e6878dc68503aef24a7bc19ea
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-10-24 15:34:48 +0530

    "ads1_m19_a1: 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/m19/19.1 BST/Solution.java
+++ Assignments/m19/19.1 BST/Solution.java
@@ -1,14 +1,15 @@
 import java.util.Scanner;
 
 final class Solution {
-    /**
+    /**.
      * Private constructor
      */
     private Solution() {
         //private constructor
     }
-    /**
+    /**.
      * main method for handling the input testcases
+     * Complexity is O(N) as it reads the input
      *
      * @param      args  The arguments
      */
@@ -69,38 +70,80 @@ final class Solution {
         }
     }
 }
-
+/**.
+ * Class to representthe book properties
+ */
 class BookInfo implements Comparable<BookInfo> {
-    
+    /**.
+     * variable for BookName
+     */
     private String bookName;
-    
+    /**.
+     * variable for bookAuthor
+     */
     private String bookAuthor;
-    
+    /**.
+     * variable for book price
+     */
     private String bookPrice;
-    
+    /**
+     * bookInfo constructor
+     *
+     * @param      n     { as name }
+     * @param      a     { as author }
+     * @param      p     { as price }
+     */
     BookInfo(final String n, final String a, final String p) {
         this.bookName = n;
         this.bookAuthor = a;
         this.bookPrice = p;
     }
-   
+    /**.
+     * method to get the name
+     * Complexity is O(1)
+     *
+     * @return     The name of type string
+     */
     public String getName() {
         return this.bookName;
     }
-    
+    /**.
+     * method to get author name
+     * Complexity is O(1)
+     *
+     * @return     The author of type string
+     */
     public String getAuthor() {
         return this.bookAuthor;
     }
-    
+    /**.
+     * method to get the price
+     * Complexity is O(1)
+     *
+     * @return     The price.
+     */
     public String getPrice() {
         return this.bookPrice;
     }
-    
+    /**.
+     * method to  print the object
+     * Complexity is O(1)
+     *
+     * @return     object as String
+     */
     public String toString() {
         return this.getName() + ", "
                + this.getAuthor() + ", "
                + Float.parseFloat(this.getPrice());
     }
+    /**.
+     * method to compare the two objects
+     * Complexity is O(1)
+     *
+     * @param      that  The that
+     *
+     * @return     { int }
+     */
     public int compareTo(final BookInfo that) {
         return this.getName().compareTo(that.getName());
     }
