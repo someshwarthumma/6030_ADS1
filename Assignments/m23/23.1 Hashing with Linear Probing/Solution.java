4353288ff7c72bcf76404c1dc0d0433700da161c
someshwarthuma <somesh.sandeep619@msitprogram.net>
2018-10-28 19:51:28 +0530

    "ads1_m23_a1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/m23/23.1 Hashing with Linear Probing/Solution.java
+++ Assignments/m23/23.1 Hashing with Linear Probing/Solution.java
@@ -24,13 +24,15 @@ public final class Solution {
             case "put": l.put(temp[1], Integer.parseInt(temp[2]));
                 break;
             case "display":
-
-                String str = "";
-                for (String s1 : l.keys()) {
-                    str += s1 + ":" + l.get(s1) + ", ";
+                if (l.size() == 0) {
+                    System.out.println("{}");
+                } else {
+                    String str = "";
+                    for (String s1 : l.keys()) {
+                        str += s1 + ":" + l.get(s1) + ", ";
+                    }
+                    System.out.println("{" + str.substring(0, str.length() - 2) + "}");
                 }
-                System.out.println("{" + str.substring(0, str.length() - 2) + "}");
-
 
                 break;
             case "delete":
